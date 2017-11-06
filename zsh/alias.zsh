8c29d64b75a3aa5ff7aac83f31c68c97c35023ad
RyotaSugawara <RyotaSugawara@users.noreply.github.com>
2017-11-06 19:35:56 +0900

    update alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,2 +1,3 @@
 alias cr='cd $(ghq root)/$(ghq list | peco)'
 alias op='open https://$(ghq list | peco)'
+alias gch='git checkout $(git branch -a | peco --prompt "GIT BRANCH" | head -n 1 | sed -e "s/^\*\s*//g")'
