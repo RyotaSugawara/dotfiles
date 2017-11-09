b63371671484e2eb74fc9547f2e1639704e5c1ce
RyotaSugawara <RyotaSugawara@users.noreply.github.com>
2017-11-09 14:47:57 +0900

    fix

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,4 +1,4 @@
 alias cr='cd $(ghq root)/$(ghq list | peco --prompt "GIT REPOGITORY >")'
-alias op='open https://$(ghq list | peco --prompot "GIT REPOGITORY")'
+alias op='open https://$(ghq list | peco --prompt "GIT REPOGITORY")'
 alias gch='git checkout $(git branch | sed -e "s/^\*\s*//g" | awk "{print $1}" | peco --prompt "GIT BRANCH >")'
 alias gcha='git checkout $(git branch -a | sed -e "s/^\*\s*//g" | grep -v "HEAD" | awk "{print $1}" | peco --prompt "GIT BRANCH >")'
