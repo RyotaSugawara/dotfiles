856c4eca267a61b54833aea7447175fdff7af0a8
sugawara-ryota <sugawara_ryota@cyberagent.co.jp>
2021-08-19 22:16:47 +0900

    set direnv

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 ln -sf $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
 
 # setup nvim
-mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config
 ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -sf $HOME/dotfiles/direnv $HOME/.config/direnv
 
 # setup git
 ln -sf $HOME/dotfiles/git/gitignore_global $HOME/.gitignore_global
