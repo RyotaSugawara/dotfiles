706dfec5d47ea25abf4d07bb5437f1501eea1f27
ryo9ra <ryo.suga.dev@gmail.com>
2022-05-02 13:41:31 +0900

    add tmux.conf

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -sf $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
 mkdir -p $HOME/.config
 ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
 ln -sf $HOME/dotfiles/direnv $HOME/.config/direnv
+ln -sf $HOME/dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
 
 # setup git
 ln -sf $HOME/dotfiles/git/gitignore_global $HOME/.gitignore_global
